2773f3a69ecde392629ba0d90ac92a1a3e96ffdd
VarshaPandey24 <pandeyv6639@gmail.com>
2024-08-12 23:42:06 +0530

    url added

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+       origin:["https://flashcard-website-nxex.vercel.app/"],
+       methods:["post","get"]
+       
+}));
 app.use(express.json());
 
 // MySQL Connection
